600de2e329640b672ba08b1ab93e46a3fe432b61
Robert Szachnowski <rafeq1241@gmail.com>
2022-03-30 22:24:31 +0200

    feat: processor augmentations to support value changes

--- src/processor.ts
+++ src/processor.ts
@@ -1,5 +1,7 @@
+
+import _ from 'lodash';
 import { Writable, writable } from "svelte/store";
-import { ConventionalCommentMatch, EmptyMatch, IOptions } from "./types";
+import { ConventionalCommentMatch, EmptyMatch, IItem, IOptions } from "./types";
 import { triggerInputChange as triggerInputChangeIfNeeded } from './utils';
 
 export class ConventionalCommentProcessor {
@@ -12,13 +14,15 @@ export class ConventionalCommentProcessor {
 
   private _input: HTMLTextAreaElement | HTMLInputElement;
   private _options: IOptions;
+
   private _regex: RegExp = /^\*{0,2}(?<label>[\w]+)[\s]?(?<decorations>\([\w\s\,\-]+\))?(:\*{0,2})/;
+
   private readonly _value: Writable<ConventionalCommentMatch>;
 
   constructor(input: HTMLTextAreaElement | HTMLInputElement, options: IOptions) {
     this._input = input;
     this._options = options;
-    this._value = writable<ConventionalCommentMatch>(EmptyMatch());
+    this._value = writable<ConventionalCommentMatch>(this.findLabelAndDecorationsInInput());
 
     // listen for value changes on input
     this.changeLabelAndDecorationWhenInputMatches();
@@ -27,74 +31,69 @@ export class ConventionalCommentProcessor {
     this._value.subscribe(newVal => {
       return this.matchTextAreaInputWithSelectedValue(newVal);
     });
-    this._value.subscribe(newVal => console.log(`New value in processor: ${JSON.stringify(newVal)}`));
+
+    // this._value.subscribe(newVal => console.log(`New value in processor: ${JSON.stringify(newVal)}`));
   }
 
   private changeLabelAndDecorationWhenInputMatches(): void {
     this._input.addEventListener("input", () => {
-      let regexResult = this.findLabelAndDecorationsInInput();
-      if (regexResult?.groups != null) {
-        let selectedDecorations = this._options.decorations
-          .filter(item => Array.from(regexResult.groups.decorations ?? []).includes(item.value));
-        let selectedLabel = this._options.labels
-          .find(x => x.value === regexResult.groups.label ?? "");
-
-        let newValue: ConventionalCommentMatch = {
-          label: selectedLabel,
-          decorations: selectedDecorations
-        };
-        this._value.update(_ => newValue);
-      } else {
-        this._value.update(_ => EmptyMatch());
-      }
+      let match = this.findLabelAndDecorationsInInput();
+      this._value.update(() => match);
     });
   }
 
 
   private matchTextAreaInputWithSelectedValue(value: ConventionalCommentMatch): void {
     let result = this.findLabelAndDecorationsInInput();
+    if (_.isEqual(result, value)) {
+      return;
+    }
 
-    let labelInInput = result?.groups?.label ?? "";
-    let isIncomingLabelDifferentThanInInput = !!value?.label?.value && labelInInput != value?.label?.value;
-
-    let foundDecorationsInInput = Array.from(result?.groups?.decorations ?? []);
-    let newDecors = value?.decorations?.map(newDecor => newDecor.value) ?? [];
-    let isAnyDecorationDifferent = foundDecorationsInInput
-      .filter(inputDecor => newDecors.includes(inputDecor) == false)
-      .length > 0;
-
-    let input = this._input.value;
-    if (isIncomingLabelDifferentThanInInput || isAnyDecorationDifferent) {
-      if (labelInInput.length > 0 || isIncomingLabelDifferentThanInInput) {
-        let startPosition = input.indexOf(labelInInput);
-        let endPosition = startPosition + labelInInput.length;
-        let newLabel = value?.label?.value;
-
-        if (newLabel == null) {
-          input = input.substring(endPosition + ":".length);
-        } else {
-          input = input.substring(0, startPosition) + newLabel + input.substring(endPosition);
-        }
-
-        if (endPosition == 0 && input.length > 0) {
-          input = input + ":";
-        }
+    let inputValue = this.getTextToMatch();
+    const restOfInput = this._input.value.substring(inputValue.length);
+    const valueBeingReplaced = inputValue.match(this._regex);
+    const valueToReplaceWith = this.composeLabelAndDecorationsString(value);
+    if (_.isEmpty(valueBeingReplaced)) {
+      if (inputValue.startsWith(" ") == false) {
+        inputValue = " " + inputValue;
       }
+      inputValue = `${valueToReplaceWith}${inputValue}${restOfInput}`;
+    } else {
+      inputValue = inputValue.replace(valueBeingReplaced[0], valueToReplaceWith);
+      inputValue += restOfInput;
+    }
 
-      if (foundDecorationsInInput.length > 0) {
-        let startPosition = input.indexOf(foundDecorationsInInput[0]);
-        let lastDecoration = foundDecorationsInInput[foundDecorationsInInput.length - 1];
-        let endPosition = input.indexOf(lastDecoration) + lastDecoration.length;
-        let decorsString = newDecors.join(",");
+    triggerInputChangeIfNeeded(this._input, inputValue);
+  }
 
-        input = input.substring(0, startPosition) + decorsString + input.substring(endPosition);
-      }
+  private composeLabelAndDecorationsString(match: ConventionalCommentMatch): string {
+    let label = match.label?.value ?? "";
+    let decorations = match.decorations.map(d => d.value).join(',');
+    if (decorations.length > 0) {
+      decorations = `(${decorations})`;
+    }
 
-      triggerInputChangeIfNeeded(this._input, input);
+    if (_.isEmpty(label) && _.isEmpty(decorations)) {
+      return "";
+    }
+
+    return `**${label}${decorations}:**`;
+  }
+
+  private findLabelAndDecorationsInInput(): ConventionalCommentMatch | null {
+    const textToMatch = this.getTextToMatch();
+    const result = textToMatch.match(this._regex);
+
+    if (result?.groups != null) {
+      let label = this.findLabel(result.groups?.label);
+      let decorations = this.findDecorations(result.groups?.decorations?.replace(/[\(\)]/g, '')?.split(',') ?? []);
+      return { label, decorations };
+    } else {
+      return EmptyMatch();
     }
   }
 
-  private findLabelAndDecorationsInInput(): RegExpMatchArray | null {
+  private getTextToMatch(): string {
     let textToWatch = this._input.value;
 
     let firstEndOfLine = this._input.value.indexOf("\n");
@@ -102,7 +101,13 @@ export class ConventionalCommentProcessor {
       textToWatch = this._input.value.substring(0, firstEndOfLine);
     }
 
-    return textToWatch.match(this._regex);
+    return textToWatch;
+  }
+
+  private findDecorations(values: string[]): IItem[] {
+    return this._options.decorations.filter(decoration => values.includes(decoration.value));
+  }
+  private findLabel(value: string): IItem {
+    return this._options.labels.find(label => label.value === value);
   }
 }
-
